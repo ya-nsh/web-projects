7b5e880878727a0cace37f0179a13b9c75e57cd1
yansh <yansh2017@gmail.com>
2021-04-14 19:07:24 +0530

    In Progress

--- bank-Application/script.js
+++ bank-Application/script.js
@@ -57,9 +57,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = (movements) => {
+const displayMovements = (movements, sort = false) => {
   containerMovements.innerHTML = '';
-  movements.forEach((mov, index) => {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach((mov, index) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -206,4 +209,9 @@ const fullBalance = accounts
   .flat()
   .reduce((acc, val) => acc + val);
 
-console.log(fullBalance);
+let sortChecker = false;
+btnSort.addEventListener('click', (e) => {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sortChecker);
+  sortChecker = !sortChecker;
+});
