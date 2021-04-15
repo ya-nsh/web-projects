82ded9d1e88c83647a55c3a406440326850044e4
yansh <yansh2017@gmail.com>
2021-04-15 17:05:05 +0530

    In Progress

--- bank-Application/script.js
+++ bank-Application/script.js
@@ -144,6 +144,16 @@ updateUI(currentAccount);
 containerApp.style.opacity = 100;
 //
 
+const now = new Date();
+const day = `${now.getDate()}`.padStart(2, '0');
+const month = `${now.getMonth() + 1}`.padStart(2, '0');
+const year = now.getFullYear();
+
+const hour = now.getHours();
+const min = now.getMinutes();
+
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
 btnLogin.addEventListener('click', (e) => {
   e.preventDefault();
 
