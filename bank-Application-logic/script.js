2e1aa3349dae84d20a7feb181bb026a75e20d931
yansh <yansh2017@gmail.com>
2021-04-16 21:39:07 +0530

    Refactored

--- bank-Application-logic/script.js
+++ bank-Application-logic/script.js
@@ -84,10 +84,7 @@ const dateFormat = (date, locale) => {
     return `${daysPassed} days ago`;
   }
 
-  // const day = `${date.getDate()}`.padStart(2, '0');
-  // const month = `${date.getMonth() + 1}`.padStart(2, '0');
-  // const year = date.getFullYear();
-  // return `${day}/${month}/${year}`;
+  
 
   return new Intl.DateTimeFormat(locale).format(date);
 };
