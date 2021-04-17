0dc63534ae89031de84eac376c8b7976bf72b9be
yansh <yansh2017@gmail.com>
2021-04-17 18:00:53 +0530

    initialized degress

--- 5-clock-gui/script.js
+++ 5-clock-gui/script.js
@@ -1,5 +1,13 @@
+const secondHand = document.querySelector('.second-hand');
 setInterval(() => {
   const now = new Date();
   const sec = now.getSeconds();
-  console.log(sec);
+
+  const degSec = (sec / 60) * 360;
+  //   console.log(degSec);
+  //   console.log(sec);
 }, 1000);
+
+// at 0s -> 0 deg
+// at 60s -> 360 deg
+// So, each 1s -> 6 deg
