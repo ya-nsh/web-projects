446aa1b51e60925a40fa2fae68a4eef686e41408
yansh <yansh2017@gmail.com>
2021-04-17 18:13:34 +0530

    Added the JS logic

--- 5-clock-gui/script.js
+++ 5-clock-gui/script.js
@@ -1,11 +1,21 @@
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 setInterval(() => {
   const now = new Date();
-  const sec = now.getSeconds();
 
-  const degSec = (sec / 60) * 360;
-  //   console.log(degSec);
-  //   console.log(sec);
+  const sec = now.getSeconds();
+  const min = now.getMinutes();
+  const hrs = now.getHours();
+
+  const degreeInSec = (sec / 60) * 360 + 90;
+  const degreeInMin = (min / 60) * 360 + 90;
+  const degreeInHr = (hrs / 12) * 360 + 90;
+
+  secondHand.style.transform = `rotate(${degreeInSec}deg)`;
+  minuteHand.style.transform = `rotate(${degreeInMin}deg)`;
+  hourHand.style.transform = `rotate(${degreeInHr}deg)`;
 }, 1000);
 
 // at 0s -> 0 deg
