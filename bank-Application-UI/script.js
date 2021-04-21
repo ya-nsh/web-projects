2ed7fe7245d289908c1f7f8f4694968d87f80559
yansh <yansh2017@gmail.com>
2021-04-21 20:11:27 +0530

    Created tabbed component

--- bank-Application-UI/script.js
+++ bank-Application-UI/script.js
@@ -135,13 +135,15 @@ const tabsContent = document.querySelectorAll('.operations__content');
 //  Use event delegation instead
 tabsContainer.addEventListener('click', e => {
   const clicked = e.target.closest('.operations__tab');
-  console.log(clicked);
+ 
 
   // Ignore areas where clicks are NULL
   if (!clicked) return;
 
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
   clicked.classList.add('operations__tab--active');
+
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
 
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
