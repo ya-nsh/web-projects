5c087e9cd2108bc2ab8d5bc542b595a03e90699d
yansh <yansh2017@gmail.com>
2021-04-23 20:08:01 +0530

    DOM selections

---
+++ 6-form-validator/script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const passwordRe = document.querySelector('#password2');
+const email = document.querySelector('#email');
+
+
