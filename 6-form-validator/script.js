485ef7bf09706517f21f666a23d34028a5035506
yansh <yansh2017@gmail.com>
2021-04-23 21:44:32 +0530

    refactored field checks

--- 6-form-validator/script.js
+++ 6-form-validator/script.js
@@ -23,31 +23,48 @@ function validateEmail(email) {
   return re.test(String(email).toLowerCase());
 }
 
+const getFieldName = (input) => {
+  const modified = input.id.slice(0, 1).toUpperCase() + input.id.slice(1);
+  return modified;
+};
+
+const checkFields = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (!input.value.trim()) {
+      displayError(input, `${getFieldName(input)} is required`);
+    } else {
+      displaySuccess(input);
+    }
+  });
+};
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (!username.value) {
-    displayError(username, 'Username is required');
-  } else {
-    displaySuccess(username);
-  }
 
-  if (!email.value) {
-    displayError(email, 'Email is required');
-  } else if (!validateEmail(email.value)) {
-    displayError(email, 'Invalid email');
-  } else {
-    displaySuccess(email);
-  }
+  checkFields([username, email, password, passwordRe]);
+  //   if (!username.value) {
+  //     displayError(username, 'Username is required');
+  //   } else {
+  //     displaySuccess(username);
+  //   }
 
-  if (!password.value) {
-    displayError(password, 'Password is required');
-  } else {
-    displaySuccess(password);
-  }
+  //   if (!email.value) {
+  //     displayError(email, 'Email is required');
+  //   } else if (!validateEmail(email.value)) {
+  //     displayError(email, 'Invalid email');
+  //   } else {
+  //     displaySuccess(email);
+  //   }
 
-  if (!passwordRe.value && passwordRe !== password) {
-    displayError(passwordRe, "Passwords didn't match");
-  } else {
-    displaySuccess(passwordRe);
-  }
+  //   if (!password.value) {
+  //     displayError(password, 'Password is required');
+  //   } else {
+  //     displaySuccess(password);
+  //   }
+
+  //   if (!passwordRe.value && passwordRe !== password) {
+  //     displayError(passwordRe, "Passwords didn't match");
+  //   } else {
+  //     displaySuccess(passwordRe);
+  //   }
 });
