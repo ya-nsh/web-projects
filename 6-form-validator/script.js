5cddd0e5002c172d79df9ed37c68b3d9fb1de83c
yansh <yansh2017@gmail.com>
2021-04-23 20:35:33 +0530

    email regex checker added

--- 6-form-validator/script.js
+++ 6-form-validator/script.js
@@ -11,11 +11,43 @@ const displayError = (input, message) => {
   smallMessage.textContent = message;
 };
 
+const displaySuccess = (input) => {
+  const inputformcontrol = input.parentElement;
+
+  inputformcontrol.classList.remove('error');
+  inputformcontrol.classList.add('success');
+};
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (!username.value) {
     displayError(username, 'Username is required');
   } else {
     displaySuccess(username);
+  }
+
+  if (!email.value) {
+    displayError(email, 'Email is required');
+  } else if (!validateEmail(email.value)) {
+    displayError(email, 'Invalid email');
+  } else {
+    displaySuccess(email);
+  }
+
+  if (!password.value) {
+    displayError(password, 'Password is required');
+  } else {
+    displaySuccess(password);
+  }
+
+  if (!passwordRe.value && passwordRe !== password) {
+    displayError(passwordRe, "Passwords didn't match");
+  } else {
+    displaySuccess(passwordRe);
   }
 });
