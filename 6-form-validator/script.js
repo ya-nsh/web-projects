988c3001da6c523e871427ae051e878939603b99
yansh <yansh2017@gmail.com>
2021-04-23 20:22:38 +0530

    error check

--- 6-form-validator/script.js
+++ 6-form-validator/script.js
@@ -4,4 +4,18 @@ const password = document.querySelector('#password');
 const passwordRe = document.querySelector('#password2');
 const email = document.querySelector('#email');
 
+const displayError = (input, message) => {
+  const inputformcontrol = input.parentElement;
+  const smallMessage = inputformcontrol.querySelector('small');
+  inputformcontrol.classList.add('error');
+  smallMessage.textContent = message;
+};
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (!username.value) {
+    displayError(username, 'Username is required');
+  } else {
+    displaySuccess(username);
+  }
+});
