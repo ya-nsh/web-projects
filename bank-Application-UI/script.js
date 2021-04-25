facf67fa35b07d4b483f5ed82cd37013a102955b
yansh <yansh2017@gmail.com>
2021-04-25 19:56:08 +0530

    implemented moden smoothscroll

--- bank-Application-UI/script.js
+++ bank-Application-UI/script.js
@@ -37,3 +37,13 @@ message.classList.add('cookie-message');
 message.innerHTML = `Cookies are useful for analytics. <button class = "btn btn--close-cookie">Got it!</button>`;
 
 header.prepend(message);
+
+//Smooth Scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+// Say, smooth scroll to a section when clicked on a button
+// Modern way
+btnScrollTo.addEventListener('click', e => {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
