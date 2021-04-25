a6cbb33b1fc5d1c760a7d7068a0b2d7009d7ab3e
yansh <yansh2017@gmail.com>
2021-04-25 12:02:34 +0530

    restart to base code

--- bank-Application-UI/script.js
+++ bank-Application-UI/script.js
@@ -2,19 +2,13 @@
 
 ///////////////////////////////////////
 // Modal window
-const h1 = document.querySelector('h1');
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-
-const tabsContent = document.querySelectorAll('.operations__content');
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function (e) {
-  e.preventDefault();
+const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -24,7 +18,8 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(item => item.addEventListener('click', openModal));
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -32,50 +27,5 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
-  }
-});
-
-const header = document.querySelector('.header');
-
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-
-message.innerHTML = `Cookies are useful for analytics. <button class = "btn btn--close-cookie">Got it!</button>`;
-
-header.append(message);
-
-document.querySelector('.btn--close-cookie').addEventListener('click', () => {
-  message.parentElement.removeChild(message);
-});
-
-//Smooth Scrolling
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
-btnScrollTo.addEventListener('click', e => {
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
-
-// document.querySelectorAll('.nav__link').forEach(function (el) {
-//   el.addEventListener('click', function (e) {
-//     e.preventDefault(); //to not jump into the section temporarily
-//     console.log('LINK');
-//     const id = this.getAttribute('href');
-//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-//   });
-// });
-
-// The above is not efficient as the exact same function is attached to 3 elements.
-
-// Event delegation
-// Add event listener to common parent element
-// Determine what element originated the event
-
-document.querySelector('.nav__links').addEventListener('click', function (e) {
-  e.preventDefault();
-
-  if (e.target.classList.contains('nav__link')) {
-    const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
