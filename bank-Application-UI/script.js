054a91a1dc7320a183ff019299c64db68d4254a4
yansh <yansh2017@gmail.com>
2021-04-29 21:41:34 +0530

    implemented Lazy Loading Images

--- bank-Application-UI/script.js
+++ bank-Application-UI/script.js
@@ -204,10 +204,25 @@ allSections.forEach(function (section) {
 
 // Lazy Loading Images
 const imgTargets = document.querySelectorAll('img[data-src');
-console.log(imgTargets);
 
-const loadImg = function (entries, observer) {};
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
 
-const imgObserver = new IntersectionObserver(loadImg, {});
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '206px',
+});
 
 imgTargets.forEach(img => imgObserver.observe(img));
