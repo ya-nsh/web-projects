43fd2777061b5520001eab1c465a52b28879557e
yansh <yansh2017@gmail.com>
2021-05-10 19:50:33 +0530

    added email validator

--- 7-create-login-slider/style.js
+++ 7-create-login-slider/style.js
@@ -9,3 +9,13 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
   container.classList.remove('right-panel-active');
 });
+
+function validateEmail(input) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (re.test(input.value.trim())) {
+    displaySuccess(input);
+  } else {
+    displayError(input, 'Invalid Email');
+  }
+}
