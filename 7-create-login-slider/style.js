793e494b957fdc00f3b10b23e413912c1b07d72d
yansh <yansh2017@gmail.com>
2021-05-12 21:25:22 +0530

    error checks

--- 7-create-login-slider/style.js
+++ 7-create-login-slider/style.js
@@ -11,6 +11,20 @@ signInButton.addEventListener('click', () => {
   container.classList.remove('right-panel-active');
 });
 
+const displayError = (input, message) => {
+  const inputformcontrol = input.parentElement;
+  const smallMessage = inputformcontrol.querySelector('small');
+  inputformcontrol.classList.add('error');
+  smallMessage.textContent = message;
+};
+
+const displaySuccess = (input) => {
+  const inputformcontrol = input.parentElement;
+
+  inputformcontrol.classList.remove('error');
+  inputformcontrol.classList.add('success');
+};
+
 function validateEmail(input) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -22,4 +36,5 @@ function validateEmail(input) {
   }
 }
 
-validateEmail(email);
+// validateEmail(email);
+console.log(email);
