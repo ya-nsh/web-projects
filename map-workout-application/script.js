0dab81b1fe6df4ec67c9237889b17a8e9c005250
yansh <yansh2017@gmail.com>
2021-05-31 16:29:55 +0530

    added code for centering location using leaflet

--- map-workout-application/script.js
+++ map-workout-application/script.js
@@ -20,6 +20,18 @@ navigator.geolocation.getCurrentPosition(
 
     console.log(latitude, longitude);
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const coords = [latitude, longitude];
+    const map = L.map('map').setView(coords, 10);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
   },
 
   function () {
