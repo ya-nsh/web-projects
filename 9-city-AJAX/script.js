28ffa75e01384357cc9b3006b46bb4ee9aada554
ya-nsh <yansh2017@gmail.com>
2021-08-04 19:39:44 +0530

    fetched the JSON data

--- 9-city-AJAX/script.js
+++ 9-city-AJAX/script.js
@@ -1,2 +1,10 @@
 const endpoint =
   'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const values = [];
+
+fetch(endpoint)
+  .then((res) => res.json())
+  .then((data) => values.push(...data));
+
+const findWord = (word, city) => {};
