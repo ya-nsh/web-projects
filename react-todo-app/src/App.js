7ff44ebd370a7e6cf68dbe869d4f1d950a2beecf
ya-nsh <yansh2017@gmail.com>
2021-08-11 18:55:42 +0530

    added functionality of adding todos

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([
+    'Finish Assignment',
+    'Create a grocery list',
+    'Work on the project'
+  ]);
+
+  const [input, setInput] = useState('');
+
+  const submitVal = (e) => {
+    e.preventDefault();
+    console.log(input);
+    const todo = input.trim();
+    if (todo) {
+      setTodos([...todos, todo]);
+      console.log(todos);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+
+      <form>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button type="submit" onClick={submitVal}>
+          Add todo
+        </button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
