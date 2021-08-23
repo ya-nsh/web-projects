12d73959b358ba34e04ac6d6e65fdd6c8a2a35fe
ya-nsh <yansh2017@gmail.com>
2021-08-23 10:54:51 +0530

    added strokes and offset coords

--- 10-html-canvas/script.js
+++ 10-html-canvas/script.js
@@ -6,6 +6,7 @@ let isDrawing = false;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 50;
 
 let lastX = 0;
 let lastY = 0;
@@ -13,9 +14,19 @@ let lastY = 0;
 const draw = e => {
   if (!isDrawing) return;
   console.log(e);
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+
+  lastX = e.offsetX;
+  lastY = e.offsetY;
 };
 
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => (isDrawing = true));
+canvas.addEventListener('mousedown', () => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 canvas.addEventListener('mouseup', () => (isDrawing = false));
 canvas.addEventListener('mouseout', () => (isDrawing = false));
