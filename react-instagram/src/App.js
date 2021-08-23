d5fd5c36b3c1125f33c2ea4ff9c98bf6417ed5f5
ya-nsh <yansh2017@gmail.com>
2021-08-23 16:24:19 +0530

    added in the logo

--- react-instagram/src/App.js
+++ react-instagram/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="Instagram Logo"
+          className="app__headerImage"
+        />
+      </div>
+
+      <h1>Hello</h1>
     </div>
   );
 }
