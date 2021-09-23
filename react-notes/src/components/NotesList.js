23202e108d6b19eee641756a28cc1dfed48f2b4a
ya-nsh <yansh2017@gmail.com>
2021-09-23 12:01:17 +0530

    added styling and installed nanoid

--- react-notes/src/components/NotesList.js
+++ react-notes/src/components/NotesList.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Note from './Note';
 
 export default function NotesList() {
+  const [notes, setNotes] = useState([]);
   return (
     <div className="notes-list">
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
       <Note />
     </div>
   );
