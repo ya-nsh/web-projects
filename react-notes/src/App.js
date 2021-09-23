1953161187894b6368b318d115706ea665bc7ad2
ya-nsh <yansh2017@gmail.com>
2021-09-23 10:48:02 +0530

    created a Note instance

--- react-notes/src/App.js
+++ react-notes/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import NotesList from './components/NotesList';
 
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="App">
+      <NotesList />
+    </div>
+  );
 }
--- react-notes/src/components/Note.js
+++ react-notes/src/components/Note.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { MdDeleteForever } from 'react-icons/md';
 
 export default function Note() {
-  return <div className="note"></div>;
+  return (
+    <div className="note">
+      <span>First note</span>
+      <div className="note__footer">
+        <small>15/05/2021</small>
+        <MdDeleteForever className="note__delete" size="1.2em" />
+      </div>
+    </div>
+  );
 }
