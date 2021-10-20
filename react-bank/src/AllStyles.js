a257597a7bd91b73789f0ac64a74ddb0002a5170
ya-nsh <yansh2017@gmail.com>
2021-10-20 12:55:05 +0530

    Style changes

---
+++ react-bank/src/AllStyles.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+const AllStyles = createGlobalStyle`
+
+    :root {
+    --accent-pink: #f44e77;
+    --border-colour: #cab6f1;
+    --purple-primary: #554dde;
+    --lavender-secondary: #6a6d9e; 
+    --neutral-light: #f2f6ff;
+    --dark-primary: #16194f;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        box-sizing: border-box;
+        font-family: 'Nunito', sans-serif;
+        text-decoration: none;
+    }
+    
+    body{
+        background-color: var(--neutral-light);
+        
+        color: white;
+        font-size: 1.1rem;
+    }`;
+
+export default AllStyles;
---
+++ react-bank/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import AllStyles from './AllStyles';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AllStyles />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
